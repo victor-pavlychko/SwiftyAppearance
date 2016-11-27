1dd470492856ded7b34de015262506d6affd05a6
Victor Pavlychko <victor.pavlychko@gmail.com>
2016-11-27 20:54:07 +0200

    added `UIView.refreshSubviewAppearance` and `UIApplication.refreshAppearance` methods to reload app look after changing `UIApperance`

---
+++ SwiftyAppearance/UIViewExtensions.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewExtensions.swift
+//  SwiftyAppearance
+//
+//  Created by Victor Pavlychko on 11/27/16.
+//  Copyright © 2016 address.wtf. All rights reserved.
+//
+
+import Foundation
+
+public extension UIView {
+
+    /// Reloads UIApperance of subviews
+    func refreshSubviewAppearance() {
+        let constraints = self.constraints
+        removeConstraints(constraints)
+        for subview in subviews {
+            subview.removeFromSuperview()
+            addSubview(subview)
+        }
+        addConstraints(constraints)
+    }
+}
+
+public extension UIApplication {
+
+    /// Reloads UIApperance of the key window
+    func refreshAppearance() {
+        keyWindow?.refreshSubviewAppearance()
+    }
+}
