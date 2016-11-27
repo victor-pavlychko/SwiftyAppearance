1e4daa8b1124f348ee6224abac033990826a10c9
Victor Pavlychko <victor.pavlychko@gmail.com>
2016-11-27 20:54:29 +0200

    Added `refreshAppearance` demo

--- SwiftyAppearanceDemo/FirstViewController.swift
+++ SwiftyAppearanceDemo/FirstViewController.swift
@@ -9,4 +9,29 @@
 import UIKit
 
 class FirstViewController: AppViewController {
+    
+    @IBAction func changeAppearance(sender: UISegmentedControl) {
+
+        switch sender.selectedSegmentIndex {
+        case 0:
+            FirstNavigationController.appearance {
+                UINavigationBar.appearance {
+                    $0.barStyle = .black
+                    $0.barTintColor = UIColor(rgb: 0x2980b9)
+                    $0.tintColor = UIColor(rgb: 0xecf0f1)
+                }
+            }
+        default:
+            FirstNavigationController.appearance {
+                UINavigationBar.appearance {
+                    $0.barStyle = .black
+                    $0.barTintColor = UIColor(rgb: 0xc0392b)
+                    $0.tintColor = UIColor(rgb: 0xecf0f1)
+                }
+            }
+        }
+
+        UIApplication.shared.refreshAppearance()
+    }
+    
 }
